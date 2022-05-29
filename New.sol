88a887e1dd107045963379c55487731e053eac7b
"Ayush pahuja" <"ayush.pahuja090@gmail.com">
2022-05-29 16:09:57 +0530

    nEW_TOKEN

---
+++ New.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: UNLINCENCED
+pragma solidity >= 0.8.0;
+import "github/OpenZepplin/openzeppelin-contracts/contracts/token/ERC20.sol";
+contract myERC20 is ERC20 {
+    //Creating My personal token with limited supply 
+
+}
+
+contract ERC20FixedSupply is ERC20{
+    constructor() public {
+        totalSupply += 5000;
+
+        balance[msg.sender] += 5000;
+    }
+}
